a0c49a8bd967d0b5c9c54c6759029f3f70982bf6
Gabriel Aleman Ruiz <gabrielalemanruizsvantiger@gmail.com>
2026-05-10 20:35:20 -0600

    Instalar docker

--- src/manage.sh
+++ src/manage.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+if ! command -v docker &> /dev/null; then
+  echo "Docker no está instalado. Instalando Docker Desktop..."
+
+  sudo apt-get update
+  sudo apt install -y ./docker-desktop-amd64.deb
+
+  echo "Docker instalado correctamente."
+fi
 
 echo "¿Qué quieres hacer?"
 echo "1) Levantar servicios"
